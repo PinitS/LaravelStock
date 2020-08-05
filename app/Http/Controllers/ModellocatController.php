cd545ba04df55cca4d4947a4be110ea0b78d51ba
PinitS <zproject.4.u@gmail.com>
2020-08-05 12:39:35 +0700

    before create add modellocate

---
+++ app/Http/Controllers/ModellocatController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Modellocat;
+use Illuminate\Http\Request;
+
+class ModellocatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Modellocat  $modellocat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Modellocat $modellocat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Modellocat  $modellocat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Modellocat $modellocat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Modellocat  $modellocat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Modellocat $modellocat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Modellocat  $modellocat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Modellocat $modellocat)
+    {
+        //
+    }
+}
---
+++ database/factories/ModellocatFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Modellocat;
+use Faker\Generator as Faker;
+
+$factory->define(Modellocat::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
