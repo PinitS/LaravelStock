55cfd6b5556d5d0a1bdb3f4ed25d8a6b8d7c41d3
PinitS <zproject.4.u@gmail.com>
2020-08-31 13:07:57 +0700

    edit nav bar

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -16,59 +16,42 @@
 
 </div> -->
 
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <a class="navbar-brand position-collapse" href="{{ action('CategoryController@index') }}">Farm Thailand</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="true" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark border-bottom shadow-sm justify-content-center align-items-start">
-    <button class="navbar-toggler mr-auto" type="button" data-toggle="collapse" data-target="#navbar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <!-- notice position-absolute on the brand which allows it to center-->
-    
+  <div class="navbar-collapse collapse show" id="navbarsExample04" style="">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Calculator</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
+          <a class="dropdown-item" href="{{ action('ModelcalController@create') }}">SimulatorModels</a>
+          <a class="dropdown-item" href="/CheckSim">Requirement</a>
+        </div>
+      </li>
 
-    <div class="navbar-collapse collapse position-absolute">
-      <ul class="navbar-nav justify-content-center">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">StrockProduct</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown02">
+          <a class="dropdown-item" href="{{ action('CategoryController@create') }}">AddCategory</a>
+          <a class="dropdown-item" href="{{ action('ProductController@create') }}">AddProduct</a>
+          <a class="dropdown-item" href="/search">SearchProduct</a>
+          <a class="dropdown-item" href="/dashboard">DashBorad</a>
+          <a class="dropdown-item" href="/report">Report</a>
+        </div>
+      </li>
 
-            <li class="nav-item dropdown mr-2">
-              <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Calculator</a>
-                <div class="dropdown-menu dropdown-menu-center text-center">
-                  <a class="p-2 dropdown-item" href="{{ action('ModelcalController@create') }}">SimulatorModels</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="p-2 dropdown-item" href="/CheckSim">Requirement</a>
-                </div>
-            </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">StrockProduct</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown02">
+          <a class="dropdown-item" href="{{ action('ModellocatController@create') }}">Models</a>
+          <a class="dropdown-item" href="{{ action('BroadlocatController@create') }}">AddBroads</a>
+          <a href="https://www.shorturl.at/" target="_blank" class="dropdown-item">ShortUrl</a>
+        </div>
+      </li>
 
-            <li class="nav-item dropdown mr-2">
-              <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">StrockProduct</a>
-                <div class="dropdown-menu dropdown-menu-center text-center">
-                  <a class="p-2 dropdown-item" href="{{ action('CategoryController@create') }}">AddCategory</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="p-2 dropdown-item" href="{{ action('ProductController@create') }}">AddProduct</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="p-2 dropdown-item" href="/search">SearchProduct</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="p-2 dropdown-item" href="/dashboard">DashBorad</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="p-2 dropdown-item" href="/report">Report</a>
-                </div>
-            </li>
-
-            <li class="nav-item dropdown mr-2">
-              <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Location</a>
-                <div class="dropdown-menu dropdown-menu-center text-center">
-                  <a class="p-2 dropdown-item" href="{{ action('ModellocatController@create') }}">Models</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="p-2 dropdown-item" href="{{ action('BroadlocatController@create') }}">AddBroads</a>
-                  <div class="dropdown-divider"></div>
-                  <a href="https://www.shorturl.at/" target="_blank" class="dropdown-item">ShortUrl</a>
-                </div>
-            </li>
-
-        </ul>
-
-    </div>
-
-    <div class="navbar-collapse collapse">
-        <ul class="navbar-nav justify-content-center">
-          <a class="navbar-brand position-collapse" href="{{ action('CategoryController@index') }}">Farm Thailand</a>
-        </ul>
-    </div>
+    </ul>
+  </div>
 </nav>
